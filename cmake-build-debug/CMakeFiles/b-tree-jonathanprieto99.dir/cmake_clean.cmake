92a225fe93ce8db7748f0e2cf49d36b7b1a4fd4e
Jonathan Prieto <jonathan.prieto@me.com>
2019-11-16 12:35:22 -0500

    Files

---
+++ cmake-build-debug/CMakeFiles/b-tree-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/b-tree-jonathanprieto99.dir/main.cpp.o"
+  "b-tree-jonathanprieto99"
+  "b-tree-jonathanprieto99.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/b-tree-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
